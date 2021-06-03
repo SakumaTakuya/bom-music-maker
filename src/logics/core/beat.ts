3e7ae122265ad54b957931ed2a2c6de754b784dd
SakumaTakuya <saku-taku9922@keio.jp>
2021-06-03 12:25:47 +0900

    add beat creator

---
+++ src/logics/core/beat.ts
@@ -0,0 +1,26 @@
+import { select } from '../../utils/array';
+import { Beat } from './melody';
+
+export function selectBeats(
+  timeSigneture: [number, number],
+  leaderBoard: Beat[]
+): Beat[] {
+  const results: Beat[] = [];
+  const [pulses, measured] = timeSigneture;
+  let sum = 0;
+  while (sum < pulses * measured) {
+    const limit = pulses - sum;
+    const maxi = Math.max(...leaderBoard.filter((v) => v <= limit));
+    const beat = select(...leaderBoard.filter((v) => v <= maxi));
+
+    if (beat === undefined) {
+      break;
+    }
+
+    sum += beat;
+
+    results.push(beat);
+  }
+
+  return results;
+}
