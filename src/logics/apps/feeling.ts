28b81db43a356762967d26ea2cf2b27e8e704957
SakumaTakuya <saku-taku9922@keio.jp>
2021-06-04 23:40:31 +0900

    fix heat

--- src/logics/apps/feeling.ts
+++ src/logics/apps/feeling.ts
@@ -14,7 +14,7 @@ export type Heats = PositionalOption<Heat>[];
 
 export const accompanimentBeatLeaderBoardMap: { [index in Heat]: Beat[] } = {
   calm: [4, 2, 2, 1],
-  agitato: [2, 2, 1, 1, 0.5, 0.5, 0.25],
+  agitato: [2, 2, 1, 1, 1, 0.5, 0.5, 0.25],
 };
 
 export const melodyBeatLeaderBoardMap: { [index in Heat]: Beat[] } = {
